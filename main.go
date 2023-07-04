11bda07b1f00fc90d686d620a9b4d9a69e213329
izasoerya <ihza.surya@gmail.com>
2023-07-04 13:08:12 +0700

    controller done

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New()
-	app.Listen(":3000")
-	
+	app.Use(logger.New())
+	err := app.Listen(":3000")
+	if err != nil {
+		panic(err)
+	}
+
 }
