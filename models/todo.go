399a4c2917f0fb1fccf7c221774fd2e3c8ab206c
izasoerya <ihza.surya@gmail.com>
2023-07-05 22:18:20 +0700

    connect to db

---
+++ models/todo.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+    "time"
+)
+
+// Todo - todo model
+type Todo struct {
+    ID        *string   `json:"id,omitempty" bson:"_id,omitempty"`
+    Title     *string   `json:"title"`
+    Completed *bool     `json:"completed"`
+    CreatedAt time.Time `json:"createdAt"`
+    UpdatedAt time.Time `json:"updatedAt"`
+}
+
