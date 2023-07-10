ab466e0b5b63fde887e5eaa5a06bc6b97171b364
izasoerya <ihza.surya@gmail.com>
2023-07-10 22:06:38 +0700

    finished (2) pop up create todo

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,26 +1,37 @@
 function ShowNotification() {
     if (Notification.permission === "granted") {
-        new Notification("Hello, World!");
+        new Notification("Todo Created!");
     } else if (Notification.permission !== "denied") {
         Notification.requestPermission().then(function(permission) {
             if (permission === "granted") {
-                new Notification("Hello, World!");
+                new Notification("Todo Created!");
             } 
         });
     }
 }
 
-function createTodo() {
-    if (event.keyCode === 13 || event.key === 'Enter') {
-    fetch('/app', {
-      method: 'POST',
-    })
-    new Notification("Todo Created")
-      .catch(error => {
-        console.error('Error:', error);
-      });
-    }
-    else 
-        throw error
+function handleKeyDown(event) {
+  if (event.keyCode === 13 || event.key === 'Enter') {
+    createTodo();
+  }
 }
+
+function createTodo() {
+  fetch('/app', {
+    method: 'POST',
+  })
+  ShowNotification()
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error:', response.status);
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      // Handle the error here
+    });
+}
+
+
+
   
