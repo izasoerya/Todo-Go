7bb84f842b5db117619af07cf942db04b9577398
izasoerya <ihza.surya@gmail.com>
2023-07-14 00:52:54 +0700

    add function to read button for input

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,24 +1,60 @@
 function buttonCreate() {
-  alert("Todo Created!")
+	submitForm("Create")
 }
 function buttonDelete() {
-  alert("Todo Deleted!")
+	submitForm("Delete")
 }
 function buttonEdit() {
-  alert("Todo Edited!")
+	submitForm("Edit")
 }
 function buttonShow() {
-  alert("Todo Showed!")
+	submitForm("Show")
 }
+
 function handleKeyDown(event) {
-  if (event.keyCode === 13 || event.key === 'Enter') {
-      alert("Todo Created!")
-  }
+	if (event.keyCode === 13 || event.key === 'Enter') {
+	//DO SOMETHING
+	}
 }
-function readMessage() {
-  fetch('./api/Todos/createTodo')
-      .then((response) => response.json())
-  alert(response)
+
+function submitForm(iterHandler) {
+	const titleInput = document.getElementById('inputTodos');		//Get input from input HTML
+	const titleValue = titleInput.value;		
+	
+	const createData = {
+		title: titleValue		//Make json {"title":titleValue}
+	};
+	const deleteData = 2
+	const showData = 2
+	const editData = 2
+	
+	switch(iterHandler) {
+		case "Create":
+			createHandler(createData)
+			break;
+		case "Delete":
+			createHandler(deleteData)
+			break;
+		case "Edit":
+			createHandler(editData)
+			break;
+		case "Show":
+			createHandler(showData)
+			break;
+	}
+}
+
+function createHandler(createData) {
+	fetch('/api/Todos/CreateTodoPage', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(createData)		//Convert JavaScript object to JSON string
+		})
+	.catch(error => {
+		console.error('Error:', error);
+	});
 }
 
 
